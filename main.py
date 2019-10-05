1c6a4b289881e4742ab55432f6f53981e0da9e12
Madison McMullen <madisonmcmullen@Madisons-MacBook-Pro-2.local>
2019-10-04 21:20:54 -0400

    Space shooter changes

--- main.py
+++ main.py
@@ -1,1 +1,254 @@
+import os, sys, logging, arcade, random
 
+#check to make sure we are running the right version of Python
+version = (3,7)
+assert sys.version_info >= version, "This script requires at least Python {0}.{1}".format(version[0],version[1])
+
+#turn on logging, in case we have to leave ourselves debugging messages
+logging.basicConfig(format='[%(filename)s:%(lineno)d] %(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT =900
+MARGIN = 20
+SCREEN_TITLE = "Aliens and Donuts"
+NUM_DONUTS = 8
+MOVEMENT_SPEED = 5
+INITIAL_VELOCITY = 2
+FRICTION = 0.9
+
+STARTING_LOCATION = (600,75)
+BULLET_DAMAGE = 50
+ENEMY_HP = 150
+BOSS_HP = 500
+KILL_SCORE = 2000
+HIT_SCORE = 100
+
+# goal of game is to move an alien around via keyboard, shooting with spacebar at sky donuts
+
+class Bullet(arcade.Sprite):
+    def __init__(self, position, velocity, damage):
+        ''' 
+        initializes the bullet
+        Parameters: position: (x,y) tuple
+            velocity: (dx, dy) tuple
+            damage: int (or float)
+        '''
+        super().__init__("images/bullet.png", 0.4)
+        (self.center_x, self.center_y) = position
+        (self.dx, self.dy) = velocity
+        self.damage = damage
+        #makes damage an attribute of the bullet
+
+    def update(self):
+        '''
+        Moves the bullet
+        '''
+        self.center_x += self.dx
+        self.center_y += self.dy
+
+class Player(arcade.Sprite):
+    def __init__(self):
+        super().__init__("images/alien.png", 1.25)
+        #super is whatever arcade.sprite needs to initialize itself, do it
+        (self.center_x, self.center_y) = STARTING_LOCATION
+        self.moving_left = False
+        self.moving_right = False
+
+class Donut(arcade.Sprite):
+    def __init__(self, position, velocity):
+        '''
+        initializes a penguin enemy
+        Parameter: position: (x,y) tuple
+        '''
+        self.donuts = ["images/donut1.png", "images/donut2.png","images/donut3.png", "images/donut4.png", "images/donut5.png"]
+        donut = random.choice(self.donuts)
+        super().__init__(donut, 1.0)
+        self.hp = ENEMY_HP
+        (self.center_x, self.center_y) = position
+        (self.dx, self.dy) = velocity
+
+    def update(self):
+        self.center_x = self.center_x + self.dx
+        self.center_y = self.center_y + self.dy
+        if self.center_x <= 0:
+            self.dx = abs(self.dx)            
+        if self.center_x >= SCREEN_WIDTH:
+            self.dx = abs(self.dx) * -1
+        if self.center_y <= 750:
+            self.dy = abs(self.dy)
+        if self.center_y >= SCREEN_HEIGHT:
+            self.dy = abs(self.dy) * -1
+
+class Second_level_donut(arcade.Sprite):
+    def __init__(self, position, velocity):
+        self.donuts = ["images/donut1.png", "images/donut2.png","images/donut3.png", "images/donut4.png", "images/donut5.png"]
+        donut = random.choice(self.donuts)
+        super().__init__(donut, 1.0)
+        self.hp = ENEMY_HP
+        (self.center_x, self.center_y) = position
+        (self.dx, self.dy) = velocity
+
+    def update(self):
+        self.center_x = self.center_x + self.dx
+        self.center_y = self.center_y + self.dy
+        if self.center_x <= 0:
+            self.dx = abs(self.dx)            
+        if self.center_x >= SCREEN_WIDTH:
+            self.dx = abs(self.dx) * -1
+        if self.center_y <= 750:
+            self.dy = abs(self.dy)
+        if self.center_y >= SCREEN_HEIGHT:
+            self.dy = abs(self.dy) * -1
+
+#class Third_level_donut(arcade.Sprite):
+ #   def __init__(self, position, velocity):
+  #      self.donuts = ["images/Boss_donut.png"]
+    #     super().__init__(donut, 1.0)
+    #    self.hp = BOSS_HP
+     #   (self.center_x, self.center_y) = position
+      #  (self.dx, self.dy) = velocity
+
+    def update(self):
+        self.center_x = self.center_x + self.dx
+        self.center_y = self.center_y + self.dy
+        if self.center_x <= 0:
+            self.dx = abs(self.dx)            
+        if self.center_x >= SCREEN_WIDTH:
+            self.dx = abs(self.dx) * -1
+        if self.center_y <= 750:
+            self.dy = abs(self.dy)
+        if self.center_y >= SCREEN_HEIGHT:
+            self.dy = abs(self.dy) * -1
+
+
+
+class Window(arcade.Window):
+
+    def __init__(self, width, height, title):
+
+        # Call the parent class's init function
+        super().__init__(width, height, title)
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
+        
+        # Make the mouse disappear when it is over the window.
+        # So we just see our object, not the pointer.
+        self.set_mouse_visible(False)
+
+        self.background = None
+        
+        self.boss= False
+
+
+    def setup(self):
+
+        #sprite lists
+        self.player_list = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
+        self.donut_list = arcade.SpriteList()
+
+        #set up the player
+        self.score = 0
+        self.player = Player()
+        self.player_list.append(self.player)
+
+        self.background = arcade.load_texture("images/background.jpg")
+
+        for i in range(NUM_DONUTS):
+            x = 120 * (i+1)
+            y = 875
+            dx = INITIAL_VELOCITY
+            dy = INITIAL_VELOCITY
+            donut = Donut((x,y), (dx,dy))
+            self.donut_list.append(donut)        
+
+
+    def update(self, delta_time):
+
+        if self.player.moving_left:
+            self.player.center_x = self.player.center_x - MOVEMENT_SPEED
+            if self.player.center_x <= 0:
+                self.player.center_x = 0
+        if self.player.moving_right:
+            self.player.center_x = self.player.center_x + MOVEMENT_SPEED
+            if self.player.center_x >= SCREEN_WIDTH:
+                self.player.center_x = SCREEN_WIDTH
+
+        self.donut_list.update()
+        for e in self.donut_list:
+            e.update()
+
+            damage = arcade.check_for_collision_with_list(e,self.bullet_list)
+            for d in damage:
+                e.hp = e.hp - d.damage              
+                d.kill()
+                if e.hp < 0:
+                    e.kill()
+                    self.score = self.score + KILL_SCORE
+                else:
+                    self.score = self.score + HIT_SCORE
+
+        if len(self.donut_list) == 0:
+            velocities_x = [3,-3]
+            velocities_y = [3,-3]
+            for i in range(NUM_DONUTS):
+                x = random.randint(MARGIN, SCREEN_WIDTH - MARGIN)
+                y = 875
+                dx = random.choice(velocities_x)
+                dy = random.choice(velocities_y)
+                donut = Second_level_donut((x,y), (dx,dy))
+                self.donut_list.append(donut)
+
+
+        self.bullet_list.update()
+
+
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        arcade.start_render()
+
+        arcade.draw_texture_rectangle(SCREEN_WIDTH//2, SCREEN_HEIGHT//2, SCREEN_WIDTH, SCREEN_HEIGHT,self.background)
+
+        # Draw all the sprites.
+        self.donut_list.draw()
+        self.player_list.draw()
+        self.bullet_list.draw()
+
+        # Render the text
+        arcade.draw_text(f"Score: {self.score}", 10, 20, arcade.color.WHITE, 14)
+       
+
+    def on_key_press(self, key, modifiers):
+        """ Called whenever the user presses a key. """
+        if key == arcade.key.LEFT:
+            self.player.moving_left = True
+        elif key == arcade.key.RIGHT:
+            self.player.moving_right = True
+        elif key == arcade.key.SPACE:
+            x = self.player.center_x
+            y = self.player.center_y + 15
+            bullet = Bullet((x,y),(0,15),BULLET_DAMAGE)
+            self.bullet_list.append(bullet)
+
+    def on_key_release(self, key, modifiers):
+        """ Called whenever a user releases a key. """
+        if key == arcade.key.LEFT:
+            self.player.moving_left = False
+        if key == arcade.key.RIGHT:
+            self.player.moving_right = False
+    
+
+
+def main():
+    window = Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
